5b89ece82a9beef37c909613db22f5e21b64afd5
Aminul667 <rahathssn@gmail.com>
2023-02-09 18:14:12 +0600

    added other sections

--- milistone_5/module_25/js/dom.js
+++ milistone_5/module_25/js/dom.js
@@ -1,5 +1,20 @@
-const liCollection = document.getElementsByTagName("li");
+// const hByID = document.getElementById('new-header');
+// console.log(hByID);
+// console.log(hByID.innerText)
+// hByID.innerText = 'This is changed Head';
+// console.log(hByID.innerText);
 
-for (const li of liCollection) {
-  console.log(li.innerText);
+const sections = document.querySelectorAll('section');
+
+for(const section of sections){
+    section.style.border = '2px solid steelblue';
+    section.style.marginBottom = '5px';
+    section.style.borderRadius = '15px';
+    section.style.padding = '15px';
+    section.style.backgroundColor = 'lightgray';
 }
+
+const colorSection = document.getElementById('color-section');
+colorSection.style.backgroundColor = 'lightblue';
+
+colorSection.classList.remove('text-center');
