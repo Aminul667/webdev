8f03eec067895e7858494f0c31d9c77e25302825
Aminul667 <rahathssn@gmail.com>
2023-02-10 11:16:19 +0600

    added 26-2

--- milistone_5/module_26/js/evnet.js
+++ milistone_5/module_26/js/evnet.js
@@ -1,1 +1,16 @@
 console.log("This is working");
+
+// option 2:
+function makeRed() {
+  document.body.style.backgroundColor = "red";
+}
+
+// option 3:
+function makeBlue() {
+  document.body.style.backgroundColor = "blue";
+}
+
+const makeBlueButton = document.getElementById("make-blue");
+makeBlueButton.onclick = makeBlue;
+
+console.log(makeBlueButton);
