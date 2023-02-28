077596e566a779708694f46a3eececfdf36ac076
Aminul667 <rahathssn@gmail.com>
2023-02-28 19:22:38 +0600

    34-4 (advanced) Call dynamic API, load dynamic data to display

--- milistone_6/module_34/js/countries.js
+++ milistone_6/module_34/js/countries.js
@@ -1,5 +1,3 @@
-console.log("conutries.js");
-
 const loadCountries = () => {
   fetch("https://restcountries.com/v3.1/all")
     .then((res) => res.json())
@@ -10,15 +8,31 @@ const displayCountries = (countries) => {
   const countriesContainer = document.getElementById("all-countries");
 
   countries.forEach((country) => {
-    console.log(country.name.common);
     const countryDiv = document.createElement("div");
     countryDiv.classList.add("country");
     countryDiv.innerHTML = `
         <h3>Name: ${country.name.common}</h3>
         <p>Capital: ${country.capital ? country.capital[0] : "No Country"}</p>
+        <button onclick="loadCountryDetails('${country.cca2}')">Details</button>
     `;
     countriesContainer.appendChild(countryDiv);
   });
+};
+
+const loadCountryDetails = (code) => {
+  const url = `https://restcountries.com/v3.1/alpha/${code}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => showCountryDetail(data[0]));
+};
+
+const showCountryDetail = (country) => {
+  const detailContainer = document.getElementById("detail-container");
+  detailContainer.innerHTML = `
+    <h3>Name: ${country.name.common}</h3>
+    <img src="${country.flags.png}">
+
+  `;
 };
 
 loadCountries();
