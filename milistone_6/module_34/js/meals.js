7a06df8a2c9fca922d950d979e051fbff682667b
Aminul667 <rahathssn@gmail.com>
2023-03-02 16:42:31 +0600

    34-7 (advanced) Create dynamic url based on click and display modal

--- milistone_6/module_34/js/meals.js
+++ milistone_6/module_34/js/meals.js
@@ -21,6 +21,9 @@ const displayMeals = meals => {
             <div class="card-body">
                 <h5 class="card-title">${meal.strMeal}Card title</h5>
                 <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <button onclick=loadMealDetail(${meal.idMeal}) type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#mealDetails">
+                  Details
+                </button>
             </div>
         </div>
         `
@@ -36,4 +39,20 @@ const searchMeals = () => {
     console.log(searchText);
 }
 
-loadMeals('rice');
+const loadMealDetail = idMeal => {
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayMealDetails(data.meals[0]));
+}
+
+const displayMealDetails = meal => {
+    document.getElementById('mealDetailsLabel').innerText = meal.strMeal;
+    const mealsDetails = document.getElementById('mealDetailsBody');
+    mealsDetails.innerHTML = `
+        <img class="img-fluid" src="${meal.strMealThumb}">
+    `;
+    console.log(meal);
+}
+
+loadMeals('fish');
